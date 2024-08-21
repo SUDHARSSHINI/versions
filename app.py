7df60c3896504e1ae8ebd2d91b5cae9cbdda0c50
SUDHARSSHINI <135975019+SUDHARSSHINI@users.noreply.github.com>
2024-08-21 09:41:57 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import sklearn
+import responsibleai
+import aif360
+import streamlit as st
+
+st.write("pandas version:", pd.__version__)
+st.write("numpy version:", np.__version__)
+st.write("scikit-learn version:", sklearn.__version__)
+st.write("responsibleai version:", responsibleai.__version__)
+st.write("aif360 version:", aif360.__version__)
+st.write("streamlit version:", st.__version__)
