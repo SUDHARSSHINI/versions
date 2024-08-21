bc65d944e51edf984359eb179c1514a347167009
SUDHARSSHINI <135975019+SUDHARSSHINI@users.noreply.github.com>
2024-08-21 10:39:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,11 @@
-import pandas as pd
-import numpy as np
-import sklearn
-import responsibleai
-import aif360
-import streamlit as st
+import subprocess
+import sys
 
-def get_versions():
-    versions = {
-        'pandas': pd.__version__,
-        'numpy': np.__version__,
-        'scikit-learn': sklearn.__version__,
-        'responsibleai': responsibleai.__version__,
-        'aif360': aif360.__version__,
-        'streamlit': st.__version__
-    }
-    return versions
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
-st.title("Package Versions Check")
-versions = get_versions()
-st.write("Installed package versions:")
-for package, version in versions.items():
-    st.write(f"{package} version: {version}")
+# List the packages you want to install or update
+packages = ['numpy==2.2.0', 'pandas==2.3.0']
+
+for package in packages:
+    install(package)
